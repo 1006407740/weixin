a049082a35838cf9aed5cecabdf1616872223cfb
ll <ll@DESKTOP-GTG4BQ0>
2019-04-03 11:19:26 +0800

    微信测试

--- src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
+++ src/main/java/org/fkjava/weixin/controller/MessageReceiverController.java
@@ -47,7 +47,7 @@ public class MessageReceiverController {
 				+"     timestamp : {}\n"//
 				+"     nonce : {}\n"//
 				+"收到的请求内容\n{}\n"//
-       );
+                ,signature,timestamp,nonce,xml );
 		return "success";
 	}
 				
